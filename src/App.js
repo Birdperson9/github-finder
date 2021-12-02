fd6e67609bcebd372c1d92e7c8faa7e427e3070b
Birdperson9 <yorieshkin@gmail.com>
2021-12-02 22:07:30 +0200

    implementing Context

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import Search from './components/users/Search'
 import Alert from './components/layout/Alert'
 import About from './components/pages/About'
 import axios from 'axios'
+
+import GithubState from './context/github/GithubState'
+
 import './App.css'
 
 const App = () => {
@@ -68,43 +71,45 @@ const App = () => {
   }
 
   return (
-    <Router>
-      <div className='App'>
-        <Navbar />
-        <div className='container'>
-          <Alert alert={alert} />
-          <Routes>
-            <Route
-              path='/'
-              element={
-                <Fragment>
-                  <Search
-                    searchUsers={searchUsers}
-                    clearUsers={clearUsers}
-                    showClear={users.length > 0 ? true : false}
-                    setAlert={showAlert}
+    <GithubState>
+      <Router>
+        <div className='App'>
+          <Navbar />
+          <div className='container'>
+            <Alert alert={alert} />
+            <Routes>
+              <Route
+                path='/'
+                element={
+                  <Fragment>
+                    <Search
+                      searchUsers={searchUsers}
+                      clearUsers={clearUsers}
+                      showClear={users.length > 0 ? true : false}
+                      setAlert={showAlert}
+                    />
+                    <Users loading={loading} users={users} />
+                  </Fragment>
+                }
+              />
+              <Route path='/about' element={<About />} />
+              <Route
+                path='/user/:login'
+                element={
+                  <User
+                    getUser={getUser}
+                    getUserRepos={getUserRepos}
+                    user={user}
+                    repos={repos}
+                    loading={loading}
                   />
-                  <Users loading={loading} users={users} />
-                </Fragment>
-              }
-            />
-            <Route path='/about' element={<About />} />
-            <Route
-              path='/user/:login'
-              element={
-                <User
-                  getUser={getUser}
-                  getUserRepos={getUserRepos}
-                  user={user}
-                  repos={repos}
-                  loading={loading}
-                />
-              }
-            />
-          </Routes>
+                }
+              />
+            </Routes>
+          </div>
         </div>
-      </div>
-    </Router>
+      </Router>
+    </GithubState>
   )
 }
 
